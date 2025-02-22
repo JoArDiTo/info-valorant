bd7282277e47cefe10c04d8a192e8d15ec89a6e4
JoArDiTo <sm.armando05@gmail.com>
2025-02-22 13:51:56 -0500

    Add Vercel adapter to support deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   output: 'server',
+
   devToolbar: {
     enabled: false
-  }
+  },
+
+  adapter: vercel()
 });
